7b265cae2785d459bf18fce424267cc43d1e0b2b
Fırat Alçın <firatalcin04@gmail.com>
2025-11-07 20:44:12 +0300

    EntityFramework Core Paketleri ve Context Sınıfı

---
+++ ZenBlogServer/Infrastructure/ZenBlogServer.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenBlogServer.Domain.Entities;
+
+namespace ZenBlogServer.Persistence.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<ContactInfo>  ContactInfos { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Social> Socials { get; set; }
+}
