1da55fc71abae4fbc1d455afbd5c01c5cf287f8c
Fırat Alçın <firatalcin04@gmail.com>
2025-11-07 23:35:11 +0300

    Secrets.json ConnectionString ve Migration

---
+++ ZenBlogServer/Infrastructure/ZenBlogServer.Persistence/Extensions/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZenBlogServer.Persistence.Context;
+
+namespace ZenBlogServer.Persistence.Extensions;
+
+public static class ServiceRegistrations
+{
+    public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlConnection"));
+        });
+    }
+}
