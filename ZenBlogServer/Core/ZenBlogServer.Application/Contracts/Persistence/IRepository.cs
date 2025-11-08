24e5615f522a1abd7336e0eb2f6114b68e97f988
Fırat Alçın <firatalcin04@gmail.com>
2025-11-08 20:53:54 +0300

    IRepository

---
+++ ZenBlogServer/Core/ZenBlogServer.Application/Contracts/Persistence/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using ZenBlogServer.Domain.Entities.Common;
+
+namespace ZenBlogServer.Application.Contracts.Persistence;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity 
+{
+    Task<List<TEntity>> GetAllAsync();
+    Task<IQueryable<TEntity>> GetQueryAsync();
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> predicate);
+    Task CreateAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
